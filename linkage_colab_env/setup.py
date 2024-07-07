960f474660da23238197e158839fc4671b6c4bcc
José Augusto <96207279+joseaugustoduarte@users.noreply.github.com>
2024-07-07 17:58:45 -0300

    update

--- linkage_colab_env/setup.py
+++ linkage_colab_env/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='linkageColabEnv',
-    version='0.1',
+    version='0.3',
     packages=['linkageColabEnv'],
     install_requires=[],
     url='https://github.com/joseaugustoduarte/linkageColabEnv',
